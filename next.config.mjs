f9c892c949abe86ab5390651de562abb42f3f50c
tmaurie <thomas.maurier91@gmail.com>
2024-12-19 12:08:34 +0100

    Init fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {{reactStrictMode: boolean, experimental: {appDir: boolean}}} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
